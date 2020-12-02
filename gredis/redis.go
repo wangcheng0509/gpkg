30d21faef9ba2de27fd3bf7d8cac1b6e28a6690a
wangcheng <wangcheng@inspiry.cn>
2020-12-02 17:33:48 +0800

    redis增加密码

--- gredis/redis.go
+++ gredis/redis.go
@@ -13,11 +13,12 @@ var RedisConn *redis.ClusterClient
 
 // Setup Initialize the Redis instance
 // redisConn: 127.0.0.1:6379|127.0.0.1:6389
-func Setup(redisConn string) {
+func Setup(redisConn, pwd string) {
 	cons := strings.Split(redisConn, "|")
 
 	RedisConn = redis.NewClusterClient(&redis.ClusterOptions{
 		Addrs:        cons,
+		Password:     pwd,
 		ReadTimeout:  50 * time.Millisecond,
 		WriteTimeout: 50 * time.Millisecond,
 	})
